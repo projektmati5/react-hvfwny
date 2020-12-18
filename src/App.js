342d770fcb906667dcd1874e4dda6aa98918ecc1
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-12-18 15:52:50 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,8 +7,22 @@ import ReactDOM from "react-dom";
 export default class App extends React.Component{
   
   constructor(props){super(props);
-  this.state={date: new Date()}}
+  this.state={dateVisible:true}}
+
+  onButtonClick()
+  {this.setState(state=>({dateVisible:!state.dateVisible}))}
   
   
-  render(){return(<button>Elo</button>)}}
+  render(){return(<div><button onClick={this.onButtonClick.bind(this)}>Elo</button>{this.state.dateVisible && <Data/>}</div>
+  )
+  }}
   
+
+class Data extends React.Component{
+  constructor(props){super(props);
+  this.state={czesc:0}}
+
+
+  render(){const dane=0;
+  return dane;}
+}
